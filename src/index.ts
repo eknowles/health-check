97266b8d94271b5f5fe780947f9849956e3c6a89
Edward Knowles <mail@eknowles.com>
2018-09-01 12:01:31 +0100

    use probot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// 2018
+import { Application } from 'probot';
+
+export = (app: Application) => {
+  // Your code here
+  app.log('Yay, the app was loaded!');
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+
+  app.on(`*`, async context => {
+    context.log({event: context.event, action: context.payload.action});
+  });
+}
