b96368fd3e1519014515fbea6e5f6be2317ddd6f
Edward Knowles <mail@eknowles.com>
2018-09-01 11:38:47 +0100

    Initial commit

---
+++ test-files/lang.js
@@ -0,0 +1,14 @@
+export default {
+  'en-gb': {
+    name: 'john doe',
+    age: 23,
+    school: 'elthorne park high school',
+    address: [
+      '1',
+      'street',
+      'city'
+    ],
+    amount: (value, arg) => value + arg,
+    stars: 'many',
+  }
+}
