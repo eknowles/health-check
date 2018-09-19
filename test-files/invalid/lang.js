3553faaeb087a61d295dc20374f4e74babc2bb94
Edward Knowles <mail@eknowles.com>
2018-09-19 15:54:10 +0100

    chore(lang): fake changes

--- test-files/invalid/lang.js
+++ test-files/invalid/lang.js
@@ -2,15 +2,16 @@ export default {
   'en-gb': {
     name: 'john doe',
     age: 23,
+    dob: '2018-09-19',
     school: 'elthorne park high school',
     address: [
-      '1',
+      '1 house',
       'street',
       'city'
     ],
     amount: (value, arg) => value + arg,
     stars: 'many',
-    alive: true,
-    test: 2,
+    alive: false,
+    test: 3,
   }
 }
