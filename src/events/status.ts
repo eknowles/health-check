4c75f8184b515c91cbcae1bd5df27125dca2289e
Edward Knowles <mail@eknowles.com>
2018-10-05 09:10:03 +0100

    feat(status): add support for any ci into github
    rename main context to `build-master` as it's smaller and nicer
    added created at timestamp to description
    added check for source status event context
    removed clearscore specific jenkins url, should work with any repo

--- src/events/status.ts
+++ src/events/status.ts
@@ -1,6 +1,10 @@
 import {Context} from 'probot';
 
-const JENKINS_URL = 'https://jenkins.spinnaker.cicd.clearscore.io';
+// add custom CI status event context here
+const TRIGGER_CONTEXTS = [
+    'test-context', // use this context when testing in dev
+    'continuous-integration/jenkins/branch'
+];
 
 /**
  * This handles the status event.
@@ -8,10 +12,11 @@ const JENKINS_URL = 'https://jenkins.spinnaker.cicd.clearscore.io';
  */
 export default async (context: Context): Promise<void> => {
     const {payload: {state, target_url, commit: {commit: {author}}, repository: {name: repo, owner: {login: owner}, default_branch: base}}} = context; // dat deconstruct tho
-    const masterBranchUrl = `${JENKINS_URL}/job/${repo}/job/${encodeURI(base)}/`;
+    const masterBranchUrl = `/job/${repo}/job/${encodeURI(base)}/`;
 
-    if (!target_url.includes(masterBranchUrl)) return;
+    if (!target_url.includes(masterBranchUrl) && !TRIGGER_CONTEXTS.includes(context.payload.context)) return;
 
+    const createdAt = new Date(context.payload.created_at).toLocaleTimeString();
     const getAllParams = {
         repo,
         owner,
@@ -24,7 +29,7 @@ export default async (context: Context): Promise<void> => {
     const prs = await context.github.pullRequests.getAll(getAllParams)
         .then((res) => res.data.map((pullRequest) => pullRequest.head.sha));
 
-    const blockedMessage = `${base} branch is being built by ${author.name}`;
+    const blockedMessage = `${author.name} @ ${createdAt} started building ${base} branch`;
     const unblockedMessage = `${base} branch is free`;
 
     const statusParams = {
@@ -32,7 +37,7 @@ export default async (context: Context): Promise<void> => {
         owner,
         state,
         target_url,
-        context: 'building-default-branch',
+        context: 'build-master',
         description: state === 'pending' ? blockedMessage : unblockedMessage,
     };
 
@@ -40,6 +45,6 @@ export default async (context: Context): Promise<void> => {
     Promise
         .all(prs.map((sha) => context.github.repos.createStatus({...statusParams, sha})))
         .then(() => {
-            context.log.info(`Master branch status set to ${state}`);
+            context.log.info(`[STATUS] updated ${prs.length} pull request(s) with the message '${base} branch status set to ${state}'`);
         });
 }
